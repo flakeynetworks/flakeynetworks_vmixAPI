d38df89ccf7c8353bef131d2e93f856d8f1859bc
Richard Stokes <richard@hamstering.co>
2018-09-25 16:31:38 +0800

    Added listeners to the TCPAPI and added close() to TCPAPI
    Now you can added listeners to the TCPAPI to notify of when specific events have occurred i.e. connected and disconnected.
    
    Also added into the TCPAPI close()

--- src/main/java/uk/co/flakeynetworks/vmix/api/TCPAPI.java
+++ src/main/java/uk/co/flakeynetworks/vmix/api/TCPAPI.java
@@ -7,6 +7,8 @@ import uk.co.flakeynetworks.vmix.api.protocol.Version20Protocol;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class TCPAPI {
@@ -14,10 +16,13 @@ public class TCPAPI {
     public static final int TCP_PORT = 8099;
 
     private VMixHost host;
+    private Socket socket;
     private OutputStreamWriter output;
     private BufferedReader input;
     private VMixTCPProtocol protocol = new Version20Protocol(this);
-    private IncomingThread listeningThread = new IncomingThread();
+    private IncomingThread listeningThread;
+
+    private Set<TCPAPIListener> listeners = new HashSet<>();
 
 
     class IncomingThread extends Thread {
@@ -31,17 +36,18 @@ public class TCPAPI {
 
                     String line = input.readLine();
                     if(line == null) {
+
+                        close();
                         // TODO Assume the connection was closed? and try to reconnect
-                        // NOTIFY A LISTENER THE CONNECTION WAS CLOSED
-                    }
+                    } // end of if
 
                     protocol.processMessage(line);
                 } catch (IOException e) {
 
                     // Attempt to reconnect
+                    close();
 
                     // TODO loop and keep trying to connect
-                    // NOTIFY A LISTENER THE CONNECTION WAS CLOSED
                     return;
                 } // end of catch
             } // end of while
@@ -55,11 +61,32 @@ public class TCPAPI {
     } // end of constructor
 
 
-    public boolean connect() {
+    public boolean addListener(TCPAPIListener listener) {
+
+        return listeners.add(listener);
+    } // end of addListener
+
+
+    public boolean removeListener(TCPAPIListener listener) {
+
+        return listeners.remove(listener);
+    } // end of removeListener
+
+
+    public void removeAllListeners() {
+        
+        listeners.clear();
+    } // end of removeAllListeners
+
+
+    public synchronized boolean connect() {
+
+        // TODO Do a check to see if we are already connected. Improve this to include other checks
+        if(socket != null) return true;
 
         try {
 
-            Socket socket = new Socket(host.getAddress(), TCP_PORT);
+            socket = new Socket(host.getAddress(), TCP_PORT);
 
             output = new OutputStreamWriter(new BufferedOutputStream(socket.getOutputStream()));
             input = new BufferedReader(new InputStreamReader(new BufferedInputStream(socket.getInputStream())));
@@ -68,23 +95,74 @@ public class TCPAPI {
 
             // TODO MAYBE SHOULD ADD A TIMEOUT HERE
             String firstResponse = input.readLine();
-            if(firstResponse == null) return false;
+            if(firstResponse == null) {
+
+                close();
+                return false;
+            } // end of if
 
             VMixVersion version = protocol.decodeVersion(firstResponse);
 
-            if(version == null) return false;
+            if(version == null) {
+
+                close();
+                return false;
+            } // end of if
+
             host.setVersion(version);
 
-            // TODO NOTIFY A LISTENER THE CONNECTION WAS OPENED
+            // Notify all the listeners that a connection was successfully made
+            for(TCPAPIListener listener: listeners)
+                listener.connected();
 
+            listeningThread = new IncomingThread();
             listeningThread.start();
         } catch (IOException e) {
 
+            close();
             return false;
         } // end of catch
 
         return true;
     } // end of connect
+
+
+    public synchronized void close() {
+
+        if(listeningThread != null) {
+
+            listeningThread.stop();
+            listeningThread = null;
+        } // end of if
+
+        if(output != null) {
+            try {
+                output.close();
+                output = null;
+            } catch (IOException ignored) {
+            } // end of catch
+        } // end of if
+
+        if(input != null) {
+            try {
+                input.close();
+                input = null;
+            } catch (IOException ignored) {
+            } // end of catch
+        } // end of if
+
+        if(socket != null) {
+            try {
+                socket.close();
+                socket = null;
+            } catch (IOException ignored) {
+            } // end of catch
+        } // end of if
+
+        // Notify all the listeners that the connection was disconnected
+        for(TCPAPIListener listener: listeners)
+            listener.disconnected();
+    } // end of close
 
 
     public void write(String writeTo) {
@@ -94,8 +172,9 @@ public class TCPAPI {
             output.flush();
         } catch (IOException ignored) {
 
+            // Restart the connection
+            close();
             ignored.printStackTrace();
-            // TODO might need to check on the network connection here.
         } // end of catch
     } // end of write
 
